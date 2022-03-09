a31c51f6438125731a84b73579f88cf70461f973
이인섭 <inseob135@gmail.com>
2022-03-09 12:49:27 +0900

    feature: Construction 빌더 추가 Construction 엔티티 빌더 추가

--- backend-java/src/main/java/com/ssafy/db/entity/Construction.java
+++ backend-java/src/main/java/com/ssafy/db/entity/Construction.java
@@ -3,10 +3,12 @@ package com.ssafy.db.entity;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 @Getter
@@ -19,4 +21,10 @@ public class Construction{
     // 회사이름
     @Column(name = "construction_name", nullable = false)
     String constructName;
+
+    @Builder
+    public Construction(Long constructionId, String constructName){
+        this.constructionId = constructionId;
+        this.constructName = constructName;
+    }
 }
