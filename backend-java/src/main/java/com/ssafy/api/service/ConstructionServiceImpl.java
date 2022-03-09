6165050a72c33bbe7d3d753738f90a7ddced5de4
이인섭 <inseob135@gmail.com>
2022-03-09 13:18:11 +0900

    feature: ConstructionServiceImpl 리스트 함수 구현 회사 리스트 받아와서 리턴하는 함수 구현

--- backend-java/src/main/java/com/ssafy/api/service/ConstructionServiceImpl.java
+++ backend-java/src/main/java/com/ssafy/api/service/ConstructionServiceImpl.java
@@ -2,6 +2,7 @@ package com.ssafy.api.service;
 
 
 import com.ssafy.api.dto.ConstructionDto;
+import com.ssafy.api.dto.UserDto;
 import com.ssafy.db.entity.Construction;
 import com.ssafy.db.repository.ConstructionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,7 +27,7 @@ public class ConstructionServiceImpl implements ConstructionService{
                 .build();
         return constructionRepository.save(construction);
     }
-/*
+
     @Override
     public List<ConstructionDto.ConstructionRes> getConstructionList(){
 
@@ -34,24 +35,11 @@ public class ConstructionServiceImpl implements ConstructionService{
 
         List<ConstructionDto.ConstructionRes> result = new ArrayList<>();
 
-        for (User user : list) {
-            if (user.getRole() == Role.INSTRUCTOR){
-                UserDto.UserRes userListRes = new UserDto.UserRes();
-
-//				userListRes.setEmail(user.getEmail());
-                userListRes.setUserName(user.getUserName());
-                userListRes.setUserNickname(user.getUserNickname());
-                userListRes.setUserDescription(user.getUserDescription());
-//				userListRes.setIsSubtitle(user.isSubtitle());
-//				userListRes.setIsCommand(user.isCommand());
-//				userListRes.setIsSTT(user.isSTT());
-//				userListRes.setIsFaceFocusing(user.isFaceFocusing());
-//				userListRes.setRole(user.getRole());
-                userListRes.setProfileImage(user.getProfileImage());
-                userListRes.setThumbnailImage(user.getThumbnailImage());
-                result.add(userListRes);
-            }
+        for (Construction construction : list) {
+            ConstructionDto.ConstructionRes constructionListRes = new ConstructionDto.ConstructionRes();
+            constructionListRes.setConstructionName(construction.getConstructName());
+            result.add(constructionListRes);
         }
         return result;
-    }*/
+    }
 }
