91eb2b55277cbce79e80ee4e262fc32d195becdf
이인섭 <inseob135@gmail.com>
2022-03-09 11:57:18 +0900

    feature: UserServiceImpl 세팅 추가 UserServiceImpl 이름, 부서 set 추가

--- backend-java/src/main/java/com/ssafy/api/service/UserServiceImpl.java
+++ backend-java/src/main/java/com/ssafy/api/service/UserServiceImpl.java
@@ -29,6 +29,8 @@ public class UserServiceImpl implements UserService {
 		user.setUserId(userRegisterInfo.getId());
 		// 보안을 위해서 유저 패스워드 암호화 하여 디비에 저장.
 		user.setUserPassword(passwordEncoder.encode(userRegisterInfo.getPassword()));
+		user.setUserName(userRegisterInfo.getName());
+		user.setUserRole(userRegisterInfo.getRole());
 		return userRepository.save(user);
 	}
 
