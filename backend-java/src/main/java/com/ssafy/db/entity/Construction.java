8907b0b94fc83c86a21c94b3740ac649f5325b97
이인섭 <inseob135@gmail.com>
2022-03-09 13:16:32 +0900

    feature: Construction @NoArgsConstructor 어노테이션 추가 JPA 에서 Entity 는 기본 생성자를 가지고 있어야 한다는 제약조건이 있는 듯 하다. No default constructor for entity 에러 해결을 위해 @NoArgsConstructor 어노테이션 추가

--- backend-java/src/main/java/com/ssafy/db/entity/Construction.java
+++ backend-java/src/main/java/com/ssafy/db/entity/Construction.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -13,6 +14,7 @@ import java.util.Date;
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
 public class Construction{
     @Id
     @Column(name = "construction_id")
