976e35136f3ddfc8fcd2f853e6956809410229fe
이인섭 <inseob135@gmail.com>
2022-03-09 12:52:04 +0900

    feature: ConstructionServiceImpl ConstructionServiceImpl 서비스 함수 구현

--- backend-java/src/main/java/com/ssafy/api/service/ConstructionServiceImpl.java
+++ backend-java/src/main/java/com/ssafy/api/service/ConstructionServiceImpl.java
@@ -7,6 +7,9 @@ import com.ssafy.db.repository.ConstructionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *	회사 등록 관련 비즈니스 로직 처리를 위한 서비스 구현 정의.
  */
@@ -17,8 +20,38 @@ public class ConstructionServiceImpl implements ConstructionService{
 
     @Override
     public Construction createConstruction(ConstructionDto.ConstructionRegisterPostReq constRegisterInfo) {
-        Construction construction = new Construction();
-        construction.setConstructName(constRegisterInfo.getConstructName());
+
+        Construction construction = Construction.builder()
+                .constructName(constRegisterInfo.getConstructName())
+                .build();
         return constructionRepository.save(construction);
     }
+/*
+    @Override
+    public List<ConstructionDto.ConstructionRes> getConstructionList(){
+
+        List<Construction> list = constructionRepository.findAll();
+
+        List<ConstructionDto.ConstructionRes> result = new ArrayList<>();
+
+        for (User user : list) {
+            if (user.getRole() == Role.INSTRUCTOR){
+                UserDto.UserRes userListRes = new UserDto.UserRes();
+
+//				userListRes.setEmail(user.getEmail());
+                userListRes.setUserName(user.getUserName());
+                userListRes.setUserNickname(user.getUserNickname());
+                userListRes.setUserDescription(user.getUserDescription());
+//				userListRes.setIsSubtitle(user.isSubtitle());
+//				userListRes.setIsCommand(user.isCommand());
+//				userListRes.setIsSTT(user.isSTT());
+//				userListRes.setIsFaceFocusing(user.isFaceFocusing());
+//				userListRes.setRole(user.getRole());
+                userListRes.setProfileImage(user.getProfileImage());
+                userListRes.setThumbnailImage(user.getThumbnailImage());
+                result.add(userListRes);
+            }
+        }
+        return result;
+    }*/
 }
