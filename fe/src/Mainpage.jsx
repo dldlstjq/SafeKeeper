a88d5c93c626161c9cfab688a407919afb7da5a1
yj <rubberboy@naver.com>
2022-03-28 14:40:30 +0900

    style:스타일링

--- fe/src/Mainpage.jsx
+++ fe/src/Mainpage.jsx
@@ -3,33 +3,50 @@ import { Box } from '@mui/system'
 import { useNavigate } from 'react-router-dom'
 import { DialogComponent } from './component/Dialogs'
 import { SignupForm, LoginForm } from './Forms'
+import logo from './images/logo-removebg.png'
+import crane from './images/crane.jpg'
 
 export default function Mainpage({ user, setUser }) {
   const navigate = useNavigate()
 
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        height: '100vh',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <DialogComponent title={'회원가입'}>
-        <SignupForm />
-      </DialogComponent>
+    <div>
+      <div style={{ display: 'flex' }}>
+        <img src={logo} alt="logo" style={{ width: '20%' }} />
+      </div>
+      <div
+        style={{
+          display: 'inline-flex',
+          backgroundImage: `url(${crane})`,
+          backgroundSize: 'contain',
+          backgroundRepeat: 'no-repeat',
+          height: '100vh',
+        }}
+      ></div>
 
-      {!user && (
-        <DialogComponent title={'로그인'}>
-          <LoginForm setUser={setUser} />
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          // height: '100vh',
+          // justifyContent: 'center',
+        }}
+      >
+        <DialogComponent title={'회원가입'}>
+          <SignupForm />
         </DialogComponent>
-      )}
-      <Bigbtn variant="contained">참가</Bigbtn>
-      <Bigbtn variant="contained" onClick={() => navigate('openvidu')}>
-        OpenVidu
-      </Bigbtn>
-    </Box>
+
+        {!user && (
+          <DialogComponent title={'로그인'}>
+            <LoginForm setUser={setUser} />
+          </DialogComponent>
+        )}
+        <Bigbtn variant="contained">참가</Bigbtn>
+        <Bigbtn variant="contained" onClick={() => navigate('openvidu')}>
+          OpenVidu
+        </Bigbtn>
+      </Box>
+    </div>
   )
 }
