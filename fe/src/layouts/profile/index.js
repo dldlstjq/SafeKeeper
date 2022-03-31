98b9c6bcab85f4fb2aeabc5c32d193974c2ffdee
방호진 <8bs4321@gmail.com>
2022-03-31 18:50:31 +0900

    style : 달력 추가

--- fe/src/layouts/profile/index.js
+++ fe/src/layouts/profile/index.js
@@ -32,7 +32,7 @@ import Footer from "examples/Footer";
 // import ProfileInfoCard from "examples/Cards/InfoCards/ProfileInfoCard";
 // import ProfilesList from "examples/Lists/ProfilesList";
 import DefaultProjectCard from "examples/Cards/ProjectCards/DefaultProjectCard";
-import PlaceholderCard from "examples/Cards/PlaceholderCard";
+// import PlaceholderCard from "examples/Cards/PlaceholderCard";
 
 // Overview page components
 import Header from "layouts/profile/components/Header";
@@ -45,10 +45,10 @@ import Header from "layouts/profile/components/Header";
 import homeDecor1 from "assets/images/home-decor-1.jpg";
 import homeDecor2 from "assets/images/home-decor-2.jpg";
 import homeDecor3 from "assets/images/home-decor-3.jpg";
-import team1 from "assets/images/team-1.jpg";
-import team2 from "assets/images/team-2.jpg";
-import team3 from "assets/images/team-3.jpg";
-import team4 from "assets/images/team-4.jpg";
+// import team1 from "assets/images/team-1.jpg";
+// import team2 from "assets/images/team-2.jpg";
+// import team3 from "assets/images/team-3.jpg";
+// import team4 from "assets/images/team-4.jpg";
 
 // 
 import * as React from 'react';
@@ -68,6 +68,12 @@ import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import SuiButton from "components/SuiButton";
 
+import TextField from '@mui/material/TextField';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import StaticDatePicker from '@mui/lab/StaticDatePicker';
+
+
 // Billing page components
 import Bill from "layouts/billing/components/Bill";
 const rows = [
@@ -81,7 +87,8 @@ const rows = [
 ];
 
 function Overview() {
-   const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = React.useState(false);
+  const [value, setValue] = React.useState(new Date());
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -90,14 +97,34 @@ function Overview() {
   const handleClose = () => {
     setOpen(false);
   };
+
   return (
     <DashboardLayout>
       <Header />
       <SuiBox mt={5} mb={3}>
         <Grid container spacing={3}>
+
+          {/* 상단부 */}
           <Grid item xs={12} xl={4}>
             <TableContainer component={Paper}>
-              <SuiBox pt={2} px={2} mb={1}>
+              {/* 달력 */}
+              <Grid item xs={12}>
+                <SuiBox pt={1} px={1} mb={1}>
+                  <LocalizationProvider dateAdapter={AdapterDateFns}>
+                    <StaticDatePicker
+                      displayStaticWrapperAs="desktop"
+                      value={value}
+                      onChange={(newValue) => {
+                        setValue(newValue);
+                      }}
+                      renderInput={(params) => <TextField {...params} />}
+                    />
+                  </LocalizationProvider>
+                </SuiBox>
+              </Grid>
+
+              {/* 다이얼로그 테스트 */}
+              {/* <SuiBox pt={2} px={2} mb={1}>
                 <SuiBox mb={0.5}>
                   <SuiTypography variant="h6" fontWeight="medium">
                     Alerts test
@@ -120,9 +147,6 @@ function Overview() {
                 >
                   test
                 </SuiButton>
-                {/* <Button variant="contained" onClick={handleClickOpen}>
-                  Open alert dialog
-                </Button> */}
               </SuiBox>
               <Dialog
                 open={open}
@@ -130,22 +154,23 @@ function Overview() {
                 aria-labelledby="alert-dialog-title"
                 aria-describedby="alert-dialog-description"
               >
-              <DialogTitle id="alert-dialog-title">
-                {"@알림창 테스트@"}
-              </DialogTitle>
-              <DialogContent>
-                <DialogContentText id="alert-dialog-description">
-                  대충 내용 어쩌구 저쩌구
-                </DialogContentText>
-              </DialogContent>
-              <DialogActions>
-                <Button onClick={handleClose}>Disagree</Button>
-                <Button onClick={handleClose} autoFocus>Agree</Button>
-              </DialogActions>
-            </Dialog>
-          </TableContainer>
+                <DialogTitle id="alert-dialog-title">{"@알림창 테스트@"}</DialogTitle>
+                <DialogContent>
+                  <DialogContentText id="alert-dialog-description">
+                    대충 내용 어쩌구 저쩌구
+                  </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                  <Button onClick={handleClose}>Disagree</Button>
+                  <Button onClick={handleClose} autoFocus>
+                    Agree
+                  </Button>
+                </DialogActions>
+              </Dialog> */}
+            </TableContainer>
           </Grid>
-          {/* 최근 알림 내역  */}
+
+          {/* 컴포넌트 테스트  */}
           <Grid item xs={12} xl={8}>
             <Card id="delete-account">
               <SuiBox pt={3} px={2}>
@@ -164,12 +189,12 @@ function Overview() {
                 </SuiBox>
               </SuiBox>
             </Card>
-            {/* <ProfilesList title="conversations" profiles={profilesListData} /> */}
           </Grid>
           
         </Grid>
       </SuiBox>
-      {/*  */}
+
+      {/* 하단부 */}
       <SuiBox mb={3}>
         <Card>
           <SuiBox pt={2} px={2}>
@@ -186,7 +211,8 @@ function Overview() {
           </SuiBox>
           <SuiBox p={2}>
             <Grid container spacing={3}>
-              <Grid item xs={12} md={6} xl={3}>
+              {/* 사고 컴포넌트..? 데이터 받아올게 있으면 3개씩 출력하면 될지도 */}
+              <Grid item xs={12} md={6} xl={4}>
                 <DefaultProjectCard
                   image={homeDecor1}
                   label="project #2"
@@ -198,15 +224,9 @@ function Overview() {
                     color: "info",
                     label: "view project",
                   }}
-                  authors={[
-                    { image: team1, name: "Elena Morison" },
-                    { image: team2, name: "Ryan Milly" },
-                    { image: team3, name: "Nick Daniel" },
-                    { image: team4, name: "Peterson" },
-                  ]}
                 />
               </Grid>
-              <Grid item xs={12} md={6} xl={3}>
+              <Grid item xs={12} md={6} xl={4}>
                 <DefaultProjectCard
                   image={homeDecor2}
                   label="project #1"
@@ -218,15 +238,9 @@ function Overview() {
                     color: "info",
                     label: "view project",
                   }}
-                  authors={[
-                    { image: team3, name: "Nick Daniel" },
-                    { image: team4, name: "Peterson" },
-                    { image: team1, name: "Elena Morison" },
-                    { image: team2, name: "Ryan Milly" },
-                  ]}
                 />
               </Grid>
-              <Grid item xs={12} md={6} xl={3}>
+              <Grid item xs={12} md={6} xl={4}>
                 <DefaultProjectCard
                   image={homeDecor3}
                   label="project #3"
@@ -238,16 +252,7 @@ function Overview() {
                     color: "info",
                     label: "view project",
                   }}
-                  authors={[
-                    { image: team4, name: "Peterson" },
-                    { image: team3, name: "Nick Daniel" },
-                    { image: team2, name: "Ryan Milly" },
-                    { image: team1, name: "Elena Morison" },
-                  ]}
                 />
-              </Grid>
-              <Grid item xs={12} md={6} xl={3}>
-                <PlaceholderCard title={{ variant: "h5", text: "New project" }} outlined />
               </Grid>
             </Grid>
           </SuiBox>
