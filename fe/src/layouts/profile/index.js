409706eea3017624e05c4bfd0e2cf9bb40717c78
방호진 <8bs4321@gmail.com>
2022-03-31 15:46:42 +0900

    feature : 프로필 초기 레이아웃

--- fe/src/layouts/profile/index.js
+++ fe/src/layouts/profile/index.js
@@ -18,9 +18,9 @@ import Grid from "@mui/material/Grid";
 import Card from "@mui/material/Card";
 
 // @mui icons
-import FacebookIcon from "@mui/icons-material/Facebook";
-import TwitterIcon from "@mui/icons-material/Twitter";
-import InstagramIcon from "@mui/icons-material/Instagram";
+// import FacebookIcon from "@mui/icons-material/Facebook";
+// import TwitterIcon from "@mui/icons-material/Twitter";
+// import InstagramIcon from "@mui/icons-material/Instagram";
 
 // Soft UI Dashboard React components
 import SuiBox from "components/SuiBox";
@@ -29,17 +29,17 @@ import SuiTypography from "components/SuiTypography";
 // Soft UI Dashboard React examples
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 import Footer from "examples/Footer";
-import ProfileInfoCard from "examples/Cards/InfoCards/ProfileInfoCard";
-import ProfilesList from "examples/Lists/ProfilesList";
+// import ProfileInfoCard from "examples/Cards/InfoCards/ProfileInfoCard";
+// import ProfilesList from "examples/Lists/ProfilesList";
 import DefaultProjectCard from "examples/Cards/ProjectCards/DefaultProjectCard";
 import PlaceholderCard from "examples/Cards/PlaceholderCard";
 
 // Overview page components
 import Header from "layouts/profile/components/Header";
-import PlatformSettings from "layouts/profile/components/PlatformSettings";
+// import PlatformSettings from "layouts/profile/components/PlatformSettings";
 
 // Data
-import profilesListData from "layouts/profile/data/profilesListData";
+// import profileZsListData from "layouts/profile/data/profilesListData";
 
 // Images
 import homeDecor1 from "assets/images/home-decor-1.jpg";
@@ -50,50 +50,126 @@ import team2 from "assets/images/team-2.jpg";
 import team3 from "assets/images/team-3.jpg";
 import team4 from "assets/images/team-4.jpg";
 
+// 
+import * as React from 'react';
+// import Table from '@mui/material/Table';
+// import TableBody from '@mui/material/TableBody';
+// import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+// import TableHead from '@mui/material/TableHead';
+// import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import SuiButton from "components/SuiButton";
+
+// Billing page components
+import Bill from "layouts/billing/components/Bill";
+const rows = [
+  {
+    name: "Frozen yoghurt",
+    calories: 159,
+    fat: 6.0,
+    carbs:24,
+    protein:4.0
+  },
+];
+
 function Overview() {
+   const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
   return (
     <DashboardLayout>
       <Header />
       <SuiBox mt={5} mb={3}>
         <Grid container spacing={3}>
-          <Grid item xs={12} md={6} xl={4}>
-            <PlatformSettings />
-          </Grid>
-          <Grid item xs={12} md={6} xl={4}>
-            <ProfileInfoCard
-              title="profile information"
-              description="Hi, I’m Alec Thompson, Decisions: If you can’t decide, the answer is no. If two equally difficult paths, choose the one more painful in the short term (pain avoidance is creating an illusion of equality)."
-              info={{
-                fullName: "Alec M. Thompson",
-                mobile: "(44) 123 1234 123",
-                email: "alecthompson@mail.com",
-                location: "USA",
-              }}
-              social={[
-                {
-                  link: "https://www.facebook.com/CreativeTim/",
-                  icon: <FacebookIcon />,
-                  color: "facebook",
-                },
-                {
-                  link: "https://twitter.com/creativetim",
-                  icon: <TwitterIcon />,
-                  color: "twitter",
-                },
-                {
-                  link: "https://www.instagram.com/creativetimofficial/",
-                  icon: <InstagramIcon />,
-                  color: "instagram",
-                },
-              ]}
-              action={{ route: "", tooltip: "Edit Profile" }}
-            />
-          </Grid>
           <Grid item xs={12} xl={4}>
-            <ProfilesList title="conversations" profiles={profilesListData} />
+            <TableContainer component={Paper}>
+              <SuiBox pt={2} px={2} mb={1}>
+                <SuiBox mb={0.5}>
+                  <SuiTypography variant="h6" fontWeight="medium">
+                    Alerts test
+                  </SuiTypography>
+                </SuiBox>
+                <SuiBox mb={1}>
+                  <SuiTypography variant="button" fontWeight="regular" color="text">
+                    show alerts
+                  </SuiTypography>
+                </SuiBox>
+                <SuiButton
+                  component="a"
+                  // href={action.route}
+                  target="_blank"
+                  rel="noreferrer"
+                  variant="outlined"
+                  size="small"
+                  color="info"
+                  onClick={handleClickOpen}
+                >
+                  test
+                </SuiButton>
+                {/* <Button variant="contained" onClick={handleClickOpen}>
+                  Open alert dialog
+                </Button> */}
+              </SuiBox>
+              <Dialog
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+              >
+              <DialogTitle id="alert-dialog-title">
+                {"@알림창 테스트@"}
+              </DialogTitle>
+              <DialogContent>
+                <DialogContentText id="alert-dialog-description">
+                  대충 내용 어쩌구 저쩌구
+                </DialogContentText>
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={handleClose}>Disagree</Button>
+                <Button onClick={handleClose} autoFocus>Agree</Button>
+              </DialogActions>
+            </Dialog>
+          </TableContainer>
           </Grid>
+          {/* 최근 알림 내역  */}
+          <Grid item xs={12} xl={8}>
+            <Card id="delete-account">
+              <SuiBox pt={3} px={2}>
+                <SuiTypography variant="h6" fontWeight="medium">
+                  넣을 내용이 없음,,
+                </SuiTypography>
+              </SuiBox>
+              <SuiBox pt={1} pb={2} px={2}>
+                <SuiBox component="ul" display="flex" flexDirection="column" p={0} m={0}>
+                  <Bill
+                    name="oliver liam"
+                    company="viking burrito"
+                    email="oliver@burrito.com"
+                    vat="FRB1235476"
+                  />
+                </SuiBox>
+              </SuiBox>
+            </Card>
+            {/* <ProfilesList title="conversations" profiles={profilesListData} /> */}
+          </Grid>
+          
         </Grid>
       </SuiBox>
+      {/*  */}
       <SuiBox mb={3}>
         <Card>
           <SuiBox pt={2} px={2}>
