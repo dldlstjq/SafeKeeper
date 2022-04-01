c711fb5586ab38f86025d88121887f77c4087e66
이인섭 <inseob135@gmail.com>
2022-04-01 15:34:06 +0900

    feature: 웹캠 화면 보여주기
    웹캠 화면 보여주는 코드 추가

--- fe/src/layouts/billing/components/BillingInformation/index.js
+++ fe/src/layouts/billing/components/BillingInformation/index.js
@@ -23,17 +23,66 @@ import SuiTypography from "components/SuiTypography";
 // Billing page components
 import Bill from "layouts/billing/components/Bill";
 
+import { useRef } from "react";
+
+// video만 가져오도록 true값을 주었습니다.
+const CONSTRAINTS = { video: true };
+
 function BillingInformation() {
+  // <video />에 걸어줄 useRef Hook을 만들어 줍니다.
+  const videoRef = useRef(null);
+
+  // navigator에 내장된 mediaDivices를 통해 연결된 미디어 장치에 접근할 수 있습니다.
+  // mediaDivice에 내장된 getUserMedia() 메서드로 MediaStream을 가져올 수 있습니다.
+  const startVideo = async () => {
+    navigator.mediaDevices
+      .getUserMedia({
+        video: { width: 1000, height: 600 },
+      })
+      .then((stream) => {
+        let video = videoRef.current;
+        video.srcObject = stream;
+        video.play();
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+    // if (videoRef && videoRef.current && !videoRef.current.srcObject) {
+    //   videoRef.current.srcObject = stream;
+    // }
+  };
+
+  const closeVideo = async () => {
+    navigator.mediaDevices.getUserMedia(null);
+  };
+
   return (
     <Card id="delete-account">
       <SuiBox pt={3} px={2}>
         <SuiTypography variant="h6" fontWeight="medium">
-          Billing Information
+          CCTV
         </SuiTypography>
       </SuiBox>
       <SuiBox pt={1} pb={2} px={2}>
         <SuiBox component="ul" display="flex" flexDirection="column" p={0} m={0}>
-          <Bill
+          <SuiBox
+            component="li"
+            display="flex"
+            justifyContent="space-between"
+            alignItems="flex-start"
+            bgColor="grey-100"
+            borderRadius="lg"
+            p={3}
+            // mb={noGutter ? 0 : 1}
+            mt={2}
+          >
+            <SuiBox width="800px" height="500px" display="flex" flexDirection="column">
+              <video autoPlay ref={videoRef} />
+              <button onClick={startVideo}>start</button>
+              <button onClick={closeVideo}>stop</button>
+            </SuiBox>
+          </SuiBox>
+          {/* <Bill
             name="oliver liam"
             company="viking burrito"
             email="oliver@burrito.com"
@@ -51,7 +100,7 @@ function BillingInformation() {
             email="ethan@fiber.com"
             vat="FRB1235476"
             noGutter
-          />
+          /> */}
         </SuiBox>
       </SuiBox>
     </Card>
