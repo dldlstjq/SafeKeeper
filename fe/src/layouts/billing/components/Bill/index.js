93773fa2e7b5eb4618ff7da6b81837845737f3f7
이인섭 <inseob135@gmail.com>
2022-04-01 15:32:30 +0900

    feature: 필요없는 부분 주석처리
    필요 없는 부분 주석 처리

--- fe/src/layouts/billing/components/Bill/index.js
+++ fe/src/layouts/billing/components/Bill/index.js
@@ -24,9 +24,7 @@ import SuiBox from "components/SuiBox";
 import SuiTypography from "components/SuiTypography";
 import SuiButton from "components/SuiButton";
 
-
 function Bill({ name, company, email, vat, noGutter }) {
-  
   return (
     <SuiBox
       component="li"
@@ -51,16 +49,16 @@ function Bill({ name, company, email, vat, noGutter }) {
             {name}
           </SuiTypography>
 
-          <SuiBox display="flex" alignItems="center" mt={{ xs: 2, sm: 0 }} ml={{ xs: -1.5, sm: 0 }}>
+          {/* <SuiBox display="flex" alignItems="center" mt={{ xs: 2, sm: 0 }} ml={{ xs: -1.5, sm: 0 }}>
             <SuiBox mr={1}>
               <SuiButton variant="text" color="error" >
                 <Icon>delete</Icon>&nbsp;delete!!!!!!!11
               </SuiButton>
             </SuiBox>
-            {/* <SuiButton variant="text" color="dark">
+            <SuiButton variant="text" color="dark">
               <Icon>edit</Icon>&nbsp;edit!!!!!!!!!!!!!
-            </SuiButton> */}
-          </SuiBox>
+            </SuiButton>
+          </SuiBox> */}
         </SuiBox>
         <SuiBox mb={1} lineHeight={0}>
           <SuiTypography variant="caption" color="text">
